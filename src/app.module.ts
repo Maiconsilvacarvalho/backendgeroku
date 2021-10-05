dd2df3cda728e93b35e2e48ec7aef4c0455a1db5
Maicon <33131590+Maiconsilvacarvalho@users.noreply.github.com>
2021-10-05 15:38:53 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://apiheroku:i_88kstb!kSJuP3@cluster0.kvtvy.mongodb.net/test',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_SERVER),
     UsersModule,
   ],
   controllers: [],
